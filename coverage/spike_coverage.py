ba87502a8537eda06e32eeab858fe7f661bd8916
indrajit <eendroroy@gmail.com>
2017-01-09 01:54:08 +0600

    add coverage

---
+++ coverage/spike_coverage.py
@@ -0,0 +1,14 @@
+import spikes
+
+
+spikes.spike([1,2,3,4,5,6,7,8], rows=0)
+spikes.spike([1,2,3,4,5,6,7,8], rows=1)
+spikes.spike([1,2,3,4,5,6,7,8], rows=2)
+spikes.spike([1,2,3,4,5,6,7,8], rows=3)
+spikes.spike([1,0,3,23,45,2,1,4,56], rows=0)
+spikes.spike([1,0,3,23,45,2,1,4,56], rows=1)
+spikes.spike([1,0,3,23,45,2,1,4,56], rows=2)
+spikes.spike([1,0,3,23,45,2,1,4,56], rows=3)
+spikes.spike([0,0,0,0], rows=2)
+spikes.spike(['a'])
+
